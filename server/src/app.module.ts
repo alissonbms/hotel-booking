f0aa6c7ceadb5ff43ddeb28a3f7a465e1162c50a
alissonbms <alissonnewdev@gmail.com>
2024-12-13 19:15:14 -0300

    chore: adjust the folders structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { validate } from "../src/env.validation";
-import { DatabaseModule } from "./database/database.module";
+import { DatabaseModule } from "./infra/database/database.module";
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, validate }), DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
