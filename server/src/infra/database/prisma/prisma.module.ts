7d31f306faf8c89dde6d1e27dc8fd53dbd96a328
alissonbms <alissonnewdev@gmail.com>
2024-12-13 21:41:29 -0300

    feat: registering repositories in the prisma module

--- server/src/infra/database/prisma/prisma.module.ts
+++ server/src/infra/database/prisma/prisma.module.ts
@@ -1,8 +1,24 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
+import { EmployeeRepository } from "@/domain/employee/repositories/employee-repository";
+import { RoomRepository } from "@/domain/employee/repositories/room-repository";
+import { BookingRepository } from "@/domain/booking/repositories/booking-repository";
+import { EmployeePrismaRepository } from "./repositories/employee-prisma-repository";
+import { RoomPrismaRepository } from "./repositories/room-prisma-repository";
+import { BookingPrismaRepository } from "./repositories/booking-prisma-repository";
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    { provide: EmployeeRepository, useClass: EmployeePrismaRepository },
+    { provide: RoomRepository, useClass: RoomPrismaRepository },
+    { provide: BookingRepository, useClass: BookingPrismaRepository },
+  ],
+  exports: [
+    PrismaService,
+    EmployeeRepository,
+    RoomRepository,
+    BookingRepository,
+  ],
 })
 export class PrismaModule {}
