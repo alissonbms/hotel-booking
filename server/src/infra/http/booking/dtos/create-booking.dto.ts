30f1233f9903f8b641756bdcd5d42d3d5ab8f33a
alissonbms <alissonnewdev@gmail.com>
2024-12-23 12:02:32 -0300

    feat: add CreateBookingDto

---
+++ server/src/infra/http/booking/dtos/create-booking.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsUUID,
+} from "class-validator";
+
+export class CreateBookingDto {
+  @IsNotEmpty({ message: "roomId should not be empty" })
+  @IsUUID("all", { message: "roomId should be a valid UUID" })
+  roomId: string;
+
+  @IsNotEmpty({ message: "Days should not be empty" })
+  @IsInt({ message: "Days should be a valid integer" })
+  days: number;
+
+  @IsNotEmpty({ message: "Customer should not be empty" })
+  customer: string;
+
+  @IsNotEmpty({ message: "Email should not be empty" })
+  @IsEmail({}, { message: "Invalid email format" })
+  email: string;
+
+  @IsOptional()
+  @IsBoolean({ message: "'Is active' field should be true or false" })
+  isActive: boolean;
+}
