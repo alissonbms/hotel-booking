21e6e93b16c27eceb33e86b3de476e3507bde4e3
alissonbms <alissonnewdev@gmail.com>
2024-12-28 01:14:35 -0300

    fix: InMemoryUploadRepository should actually implement UploadRepository and not extend it

--- server/test/repositories/in-memory-upload-repository.ts
+++ server/test/repositories/in-memory-upload-repository.ts
@@ -1,7 +1,7 @@
 import { File } from "@/domain/upload/entities/file";
 import { UploadRepository } from "@/domain/upload/repositories/upload-repository";
 
-export class InMemoryUploadRepository extends UploadRepository {
+export class InMemoryUploadRepository implements UploadRepository {
   async upload(file: File): Promise<Record<"path", string>> {
     return {
       path: "upload/" + file.name,
