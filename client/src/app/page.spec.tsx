67273f07e776bc6a0f625a3447b861df01b85a3a
alissonbms <alissonnewdev@gmail.com>
2025-02-16 22:06:44 -0300

    test: test the buttons and logo on the Home page

--- client/src/app/page.spec.tsx
+++ client/src/app/page.spec.tsx
@@ -1,8 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "./page";
 
-describe("Test Home page", () => {
-  test("Test", () => {
+describe("Test the Home page", () => {
+  test("Should have 3 buttons in Home Page", async () => {
     render(<Home />);
+
+    const buttons = await screen.findAllByRole("button");
+    const reservationBtn = screen.queryByText("Reservar quarto");
+    const consultBtn = screen.queryByText("Consultar reserva");
+    const discoveryBtn = screen.queryByText("Descobrir agora");
+
+    expect(buttons).toHaveLength(3);
+    expect(reservationBtn).toBeInTheDocument();
+    expect(consultBtn).toBeInTheDocument();
+    expect(discoveryBtn).toBeInTheDocument();
+  });
+  test("Should have a logo in Home Page", async () => {
+    render(<Home />);
+
+    const logo = await screen.findByText("Cubic Hotel");
+    const img = await screen.findByRole("img");
+    expect(logo).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
   });
 });
