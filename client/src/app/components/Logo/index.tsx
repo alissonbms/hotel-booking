1805aab4ea6a204b3c7378235900b228809a265b
alissonbms <alissonnewdev@gmail.com>
2025-02-17 22:49:37 -0300

    feat: add feature to choose logo image width and height and logo title font size

--- client/src/app/components/Logo/index.tsx
+++ client/src/app/components/Logo/index.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 
-const Logo = () => {
+interface ILogoProps {
+  size?: number;
+}
+
+const Logo = ({ size = 24 }: ILogoProps) => {
   return (
     <div className={styles.container}>
-      <h1>Cubic Hotel</h1>
-      <Image src="./assets/logo.svg" alt="Cubic Hotel" height={24} width={24} />
+      <h1
+        style={{
+          fontSize: `${size}px`,
+        }}
+      >
+        Cubic Hotel
+      </h1>
+      <Image
+        src="./assets/logo.svg"
+        alt="Cubic Hotel image logo"
+        height={size}
+        width={size}
+      />
     </div>
   );
 };
