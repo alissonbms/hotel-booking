9c97a9af9de7f8ea8e7c7fc35e6f77a1c175d203
alissonbms <alissonnewdev@gmail.com>
2025-02-17 22:20:05 -0300

    styles: add styles to Logo component

--- client/src/app/components/Logo/index.tsx
+++ client/src/app/components/Logo/index.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import styles from "./page.module.scss";
 
 const Logo = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Cubic Hotel</h1>
       <Image src="./assets/logo.svg" alt="Cubic Hotel" height={24} width={24} />
     </div>
