13a276bfd6afe2a57143967e4b63af9907c4f7e3
alissonbms <alissonnewdev@gmail.com>
2025-03-02 15:52:00 -0300

    refactor: finish the styles and structure for the Rooms page

--- client/src/app/rooms/page.tsx
+++ client/src/app/rooms/page.tsx
@@ -1,18 +1,55 @@
+import Room from "@/entities/Room";
 import Logo from "../components/Logo";
 import styles from "./page.module.scss";
+import RoomCard from "../components/RoomCard";
 
 const Rooms = () => {
+  const rooms: Room[] = [
+    {
+      id: "123456789",
+      name: "Super room",
+      price: 50000,
+      image: "/assets/hotel-room-cover.jpg",
+      hasWifi: true,
+      hasAir: true,
+      hasKitchen: true,
+      isPetFriendly: false,
+      isAvailable: false,
+    },
+    {
+      id: "123456789",
+      name: "New room",
+      price: 50000,
+      image: "/assets/hotel-room-cover.jpg",
+      hasWifi: true,
+      hasAir: true,
+      hasKitchen: true,
+      isPetFriendly: false,
+      isAvailable: false,
+    },
+  ];
   return (
-    <main className={styles.container}>
+    <main className={`${styles.container} layout`}>
       <header>
-        <Logo />
+        <div className="max-width">
+          <Logo />
+        </div>
       </header>
 
-      <h2>Available rooms</h2>
-
-      <ul>
-        <li>Super room</li>
-      </ul>
+      <div className={`${styles.content} max-width`}>
+        <h2>Available rooms</h2>
+        {rooms.length ? (
+          <ul className={`${styles["content--rooms"]} max-width`}>
+            {rooms.map((room) => (
+              <li key={room.id}>
+                <RoomCard room={room} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <span>No rooms found</span>
+        )}
+      </div>
     </main>
   );
 };
