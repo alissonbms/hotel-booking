6781c062d53d37d41ead84e86bd1e3fccf18cf0b
alissonbms <alissonnewdev@gmail.com>
2025-03-02 15:52:12 -0300

    refactor: finish the styles and structure for the RoomCard component

--- client/src/app/components/RoomCard/index.tsx
+++ client/src/app/components/RoomCard/index.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Room from "@/entities/Room";
+import Image from "next/image";
 import styles from "./styles.module.scss";
+import { Button } from "hotel-booking-components-library";
+import { formattedPriceUSD } from "@/utils/formatters";
 
 interface IRoomCardProps {
   room: Room;
@@ -8,7 +13,34 @@ interface IRoomCardProps {
 const RoomCard = ({ room }: IRoomCardProps) => {
   return (
     <div className={`${styles.container}`}>
-      <h2>{room.name}</h2>
+      <Image
+        src={room.image}
+        alt={`${room.name} image cover`}
+        width={496}
+        height={282}
+      />
+
+      <div className={styles.content}>
+        <div>
+          <h2>{room.name}</h2>
+          <div className={styles["content--options"]}>
+            {room.hasWifi && <span>&bull; Wifi</span>}
+            {room.hasAir && <span>&bull; Air-conditioning </span>}
+            {room.hasKitchen && <span>&bull; Kitchen</span>}
+            {room.isPetFriendly && <span>&bull; Pet friendly</span>}
+          </div>
+        </div>
+
+        <div className={styles["content--card-bottom"]}>
+          <div>
+            <h2>{formattedPriceUSD(room.price)}</h2>
+            <span>/ per night</span>
+          </div>
+          <Button colors="orange" variant="normal" width={200}>
+            Book room
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
