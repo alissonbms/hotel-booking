ec7be2e47823d509da46d6393d54c40156ade73b
alissonbms <alissonnewdev@gmail.com>
2025-03-05 11:13:02 -0300

    fix: fixed AssertionError in RoomList test when trying to delete an invalid room from the rooms array, waiting for it to fail

--- client/src/test/RoomList.spec.ts
+++ client/src/test/RoomList.spec.ts
@@ -103,51 +103,12 @@ describe("Test the RoomList entity", () => {
     expect(newRoomList.getRooms()).toHaveLength(2);
   });
   test("Should NOT delete a invalid room from the rooms array", () => {
-    try {
-      const newRoomList = new RoomList();
+    const newRoomList = new RoomList();
 
-      expect(newRoomList.getRooms()).toHaveLength(0);
+    expect(newRoomList.getRooms()).toHaveLength(0);
 
-      const rooms: Room[] = [
-        {
-          id: faker.database.collation(),
-          name: faker.company.name(),
-          price: parseFloat(faker.commerce.price()),
-          image: faker.image.url(),
-          hasWifi: true,
-          hasAir: true,
-          hasKitchen: true,
-          isPetFriendly: false,
-          isAvailable: true,
-        },
-        {
-          id: faker.database.collation(),
-          name: faker.company.name(),
-          price: parseFloat(faker.commerce.price()),
-          image: faker.image.url(),
-          hasWifi: true,
-          hasAir: true,
-          hasKitchen: true,
-          isPetFriendly: false,
-          isAvailable: true,
-        },
-        {
-          id: faker.database.collation(),
-          name: faker.company.name(),
-          price: parseFloat(faker.commerce.price()),
-          image: faker.image.url(),
-          hasWifi: true,
-          hasAir: true,
-          hasKitchen: true,
-          isPetFriendly: false,
-          isAvailable: true,
-        },
-      ];
-
-      newRoomList.setRooms(rooms);
-      expect(newRoomList.getRooms()).toHaveLength(3);
-
-      const unknownRoom: Room = {
+    const rooms: Room[] = [
+      {
         id: faker.database.collation(),
         name: faker.company.name(),
         price: parseFloat(faker.commerce.price()),
@@ -157,14 +118,47 @@ describe("Test the RoomList entity", () => {
         hasKitchen: true,
         isPetFriendly: false,
         isAvailable: true,
-      };
+      },
+      {
+        id: faker.database.collation(),
+        name: faker.company.name(),
+        price: parseFloat(faker.commerce.price()),
+        image: faker.image.url(),
+        hasWifi: true,
+        hasAir: true,
+        hasKitchen: true,
+        isPetFriendly: false,
+        isAvailable: true,
+      },
+      {
+        id: faker.database.collation(),
+        name: faker.company.name(),
+        price: parseFloat(faker.commerce.price()),
+        image: faker.image.url(),
+        hasWifi: true,
+        hasAir: true,
+        hasKitchen: true,
+        isPetFriendly: false,
+        isAvailable: true,
+      },
+    ];
 
-      newRoomList.deleteRoom(unknownRoom);
-      expect(newRoomList.getRooms()).toHaveLength(3);
-    } catch (error) {
-      if (error instanceof Error) {
-        expect(error.message).toBe("Room not found");
-      }
-    }
+    newRoomList.setRooms(rooms);
+    expect(newRoomList.getRooms()).toHaveLength(3);
+
+    const unknownRoom: Room = {
+      id: faker.database.collation(),
+      name: faker.company.name(),
+      price: parseFloat(faker.commerce.price()),
+      image: faker.image.url(),
+      hasWifi: true,
+      hasAir: true,
+      hasKitchen: true,
+      isPetFriendly: false,
+      isAvailable: true,
+    };
+
+    expect(() => newRoomList.deleteRoom(unknownRoom)).toThrow("Room not found");
+    expect(newRoomList.getRooms()).toHaveLength(3);
   });
 });
