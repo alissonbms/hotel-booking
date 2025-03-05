8fc90e84608a8fe5da74dcdf656d1739da21e618
alissonbms <alissonnewdev@gmail.com>
2025-03-05 10:30:47 -0300

    feat: integrate Rooms page with our api

--- client/src/app/rooms/page.tsx
+++ client/src/app/rooms/page.tsx
@@ -1,33 +1,40 @@
+"use client";
+
 import Room from "@/entities/Room";
 import Logo from "../components/Logo";
 import styles from "./page.module.scss";
 import RoomCard from "../components/RoomCard";
+import { useEffect, useRef, useState } from "react";
+import RoomsHttpGateway from "@/gateways/RoomsHttpGateway";
+import { container, Registry } from "@/infra/ContainerRegistry";
+import RoomList from "@/entities/RoomList";
 
 const Rooms = () => {
-  const rooms: Room[] = [
-    {
-      id: "123456789",
-      name: "Super room",
-      price: 50000,
-      image: "/assets/hotel-room-cover.jpg",
-      hasWifi: true,
-      hasAir: true,
-      hasKitchen: true,
-      isPetFriendly: false,
-      isAvailable: false,
-    },
-    {
-      id: "987654321",
-      name: "New room",
-      price: 50000,
-      image: "/assets/hotel-room-cover.jpg",
-      hasWifi: true,
-      hasAir: true,
-      hasKitchen: true,
-      isPetFriendly: false,
-      isAvailable: false,
-    },
-  ];
+  const render = useRef(0);
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const [roomList, setRoomList] = useState<RoomList>();
+  const [roomsHttp, setRoomsHttp] = useState<RoomsHttpGateway>();
+
+  useEffect(() => {
+    (async () => {
+      if (!roomsHttp) return;
+      if (!roomList) return;
+
+      const data = await roomsHttp.getRooms();
+      roomList.setRooms(data);
+      setRooms(data);
+    })();
+  }, [roomList, roomsHttp]);
+
+  useEffect(() => {
+    if (render.current === 0) {
+      setRoomsHttp(container.get<RoomsHttpGateway>(Registry.RoomsHttpGateway));
+      setRoomList(container.get<RoomList>(Registry.RoomList));
+
+      render.current = 1;
+    }
+  }, []);
+
   return (
     <main className={`${styles.container} layout`}>
       <header>
