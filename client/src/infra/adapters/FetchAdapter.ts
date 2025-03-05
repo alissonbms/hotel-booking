f737d7a4231f002ccf477618f8a4be504aadd719
alissonbms <alissonnewdev@gmail.com>
2025-03-05 10:25:47 -0300

    feat: create an adapter for FETCH and an adapter for AXIOS

---
+++ client/src/infra/adapters/FetchAdapter.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import IHttpClient from "../IHttpClient";
+
+export default class FetchAdapter implements IHttpClient {
+  async get(url: string): Promise<any> {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return data;
+  }
+  async post(url: string, body: any): Promise<any> {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    return data;
+  }
+  async put(url: string, body: any): Promise<any> {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    return data;
+  }
+  async delete(url: string): Promise<any> {
+    const response = await fetch(url, {
+      method: "DELETE",
+    });
+
+    const data = await response.json();
+
+    return data;
+  }
+}
