8b81cc0e394733ea3f6047a84911b9032f61986f
alissonbms <alissonnewdev@gmail.com>
2025-03-10 00:20:35 -0300

    feat: add patch method in IHttpClient

--- client/src/infra/IHttpClient.ts
+++ client/src/infra/IHttpClient.ts
@@ -3,5 +3,6 @@ export default interface IHttpClient {
   get(url: string): Promise<any>;
   post(url: string, body: any): Promise<any>;
   put(url: string, body: any): Promise<any>;
+  patch(url: string, body: any): Promise<any>;
   delete(url: string): Promise<any>;
 }
