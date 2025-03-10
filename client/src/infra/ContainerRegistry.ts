443c35a612d5fc0ff4efc8f8e53034ac286ef19a
alissonbms <alissonnewdev@gmail.com>
2025-03-10 20:58:35 -0300

    feat: configure bind for BookingsHttpGateway and BookingList in dependencies registry

--- client/src/infra/ContainerRegistry.ts
+++ client/src/infra/ContainerRegistry.ts
@@ -4,12 +4,16 @@ import FetchAdapter from "./adapters/FetchAdapter";
 import AxiosAdapter from "./adapters/AxiosAdapter";
 import RoomsHttpGateway from "@/gateways/room/RoomsHttpGateway";
 import RoomList from "@/entities/RoomList";
+import BookingsHttpGateway from "@/gateways/booking/BookingsHttpGateway";
+import BookingList from "@/entities/BookingList";
 
 export const Registry = {
   FetchAdapter: Symbol.for("FetchAdapter"),
   AxiosAdapter: Symbol.for("AxiosAdapter"),
   RoomsHttpGateway: Symbol.for("RoomsHttpGateway"),
   RoomList: Symbol.for("RoomList"),
+  BookingsHttpGateway: Symbol.for("BookingsHttpGateway"),
+  BookingList: Symbol.for("BookingList"),
 };
 
 export const container = new Container();
@@ -26,9 +30,20 @@ container.bind(Registry.RoomList).toDynamicValue(() => {
   return new RoomList();
 });
 
+container.bind(Registry.BookingList).toDynamicValue(() => {
+  return new BookingList();
+});
+
 container.bind(Registry.RoomsHttpGateway).toDynamicValue((context) => {
   return new RoomsHttpGateway(
-    context.container.get(Registry.FetchAdapter),
+    context.container.get(Registry.AxiosAdapter),
+    "http://localhost:3000",
+  );
+});
+
+container.bind(Registry.BookingsHttpGateway).toDynamicValue((context) => {
+  return new BookingsHttpGateway(
+    context.container.get(Registry.AxiosAdapter),
     "http://localhost:3000",
   );
 });
