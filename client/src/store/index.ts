4fecd1ca34e897e5316306fad67ff48c0575c489
alissonbms <alissonnewdev@gmail.com>
2025-03-10 21:05:45 -0300

    feat: add the booking slice to the global state

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,13 +1,15 @@
 import { create } from "zustand";
 import { createRoomSlices, RoomSlice } from "./slices/roomSlice";
 import { persist } from "zustand/middleware";
+import { BookingSlice, createBookingSlices } from "./slices/bookingSlice";
 
-type StoreState = RoomSlice;
+type StoreState = RoomSlice & BookingSlice;
 
 export const useAppStore = create<StoreState>()(
   persist(
     (...a) => ({
       ...createRoomSlices(...a),
+      ...createBookingSlices(...a),
     }),
     {
       name: "stateStorage",
