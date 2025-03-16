54fcd54a0f92e2b9b7f89cb1ddcd25c088c2a830
alissonbms <alissonnewdev@gmail.com>
2025-03-15 23:28:18 -0300

    refactor: add BookingUnion as a request type of the setBooking method in IBookingsGateway

--- client/src/gateways/booking/IBookingsGateway.ts
+++ client/src/gateways/booking/IBookingsGateway.ts
@@ -1,8 +1,9 @@
 import Booking from "@/entities/Booking";
+import { BookingUnion } from "../../../types/BookingUnion";
 
 export default interface IBookingsGateway {
   getBookings(): Promise<Booking[]>;
   getBooking(bookingId: string): Promise<Booking>;
-  setBooking(booking: Booking): Promise<Booking>;
+  setBooking(booking: BookingUnion): Promise<Booking>;
   cancelBooking(bookingId: string): Promise<Booking>;
 }
