e3fd6e8d8455c70e845efe4f24e56ee2e7bdd4db
alissonbms <alissonnewdev@gmail.com>
2025-03-16 00:05:42 -0300

    refactor: at the test of MyBooking page, set a current booking data and if that data is filling correctly the entries. Also verify if the checkboxs are being checked or unchecked, depending on its value (true or false)

--- client/src/app/my-booking/page.spec.tsx
+++ client/src/app/my-booking/page.spec.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, renderHook, screen } from "@testing-library/react";
 import MyBooking from "./page";
+import { useAppStore } from "@/store";
+import { faker } from "@faker-js/faker";
 
 const mockBack = vi.fn();
 
@@ -12,8 +14,34 @@ vi.mock("next/navigation", () => ({
 }));
 
 describe("Test the MyBooking page", () => {
-  test("Should call router.back() when button 'Go back' is clicked", async () => {
+  function setup() {
+    const { result } = renderHook(() => useAppStore());
+
+    result.current.setCurrentBooking({
+      id: "7c85ebd0-b522-4f25-97ee-ca6cb40fc6ea",
+      customer: "Carol",
+      email: "caraol@email.email",
+      days: 3,
+      isActive: true,
+      room: {
+        hasAir: true,
+        hasKitchen: true,
+        isPetFriendly: false,
+        hasWifi: true,
+        id: faker.database.collation(),
+        image: faker.image.url(),
+        isAvailable: false,
+        name: "Amazing room",
+        price: 450,
+      },
+    });
+
     render(<MyBooking />);
+
+    return { result };
+  }
+  test("Should call router.back() when button 'Go back' is clicked", async () => {
+    setup();
 
     const button = screen.getByText("Go back");
     fireEvent.click(button);
@@ -21,20 +49,20 @@ describe("Test the MyBooking page", () => {
     expect(mockBack).toHaveBeenCalledTimes(1);
   });
   test("Should have a title", async () => {
-    render(<MyBooking />);
+    setup();
 
     const title = screen.getByText("Bookings");
     expect(title).toBeInTheDocument();
   });
   test("Should have an image", async () => {
-    render(<MyBooking />);
+    setup();
 
     const image = await screen.findByRole("img", { name: "hotel room cover" });
 
     expect(image).toBeInTheDocument();
   });
   test("Should have a button", async () => {
-    render(<MyBooking />);
+    setup();
 
     const button = await screen.findByRole("button", {
       name: "Go back",
@@ -43,7 +71,7 @@ describe("Test the MyBooking page", () => {
     expect(button).toBeInTheDocument();
   });
   test("Should have two inputs with their labels", async () => {
-    render(<MyBooking />);
+    setup();
 
     const inputs = await screen.findAllByRole("textbox");
     const nameLabel = await screen.findByText("Name");
@@ -53,12 +81,19 @@ describe("Test the MyBooking page", () => {
     expect(nameLabel).toBeInTheDocument();
     expect(priceLabel).toBeInTheDocument();
   });
+  test("There should be the current booking data, such as room name and price per night, filling in the entries", async () => {
+    setup();
 
+    const inputs = await screen.findAllByRole("textbox");
+    expect(inputs).toHaveLength(2);
+
+    expect(inputs[0].getAttribute("value")).toBe("Amazing room");
+    expect(inputs[1].getAttribute("value")).toBe("$450.00");
+  });
   test("Should have four inputs checkbox with their labels", async () => {
-    render(<MyBooking />);
-
+    setup();
     const checkboxInputs = await screen.findAllByRole("checkbox");
-    const wifiCheckbox = await screen.findByText("Wi-Fi");
+    const wifiCheckbox = await screen.findByText("WiFi");
     const airConditioningCheckbox = await screen.findByText("Air conditioning");
     const kitchenCheckbox = await screen.findByText("Kitchen");
     const petFriendlyCheckbox = await screen.findByText("Pet friendly");
@@ -67,5 +102,31 @@ describe("Test the MyBooking page", () => {
     expect(airConditioningCheckbox).toBeInTheDocument();
     expect(kitchenCheckbox).toBeInTheDocument();
     expect(petFriendlyCheckbox).toBeInTheDocument();
+  });
+  test("Should have the wifi checkbox being checked and petFriendly checkbox being unchecked", async () => {
+    setup();
+
+    const checkboxInputs = await screen.findAllByRole("checkbox");
+
+    const wifiCheckbox = checkboxInputs[0] as HTMLElement & {
+      checked: boolean;
+    };
+    const petFriendlyCheckbox = checkboxInputs[3] as HTMLElement & {
+      checked: boolean;
+    };
+
+    expect(wifiCheckbox.getAttribute("value")).toBe("wifi");
+    expect(wifiCheckbox.checked).toBe(true);
+
+    expect(petFriendlyCheckbox.getAttribute("value")).toBe("pet-friendly");
+    expect(petFriendlyCheckbox.checked).toBe(false);
+  });
+
+  test("Should have the booking code on the screen", async () => {
+    setup();
+
+    expect(
+      screen.queryByText("7c85ebd0-b522-4f25-97ee-ca6cb40fc6ea"),
+    ).toBeInTheDocument();
   });
 });
