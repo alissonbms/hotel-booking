70790f14891041c8a24fa71a41d7325b1b36b164
alissonbms <alissonnewdev@gmail.com>
2025-03-15 23:58:08 -0300

    refactor: get the current booking data from the global state and put it in its respective places in the booking form in my-booking page (that show the booking data after you create it or search it from the booking-check page)

--- client/src/app/my-booking/page.tsx
+++ client/src/app/my-booking/page.tsx
@@ -4,9 +4,12 @@ import { Button } from "hotel-booking-components-library";
 import styles from "./page.module.scss";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useAppStore } from "@/store";
+import { formattedPriceUSD } from "@/utils/formatters";
 
 const MyBooking = () => {
   const router = useRouter();
+  const { currentBooking } = useAppStore();
   return (
     <main className={`${styles.container} layout`}>
       <div className={`${styles["container--header"]} max-width`}>
@@ -34,7 +37,7 @@ const MyBooking = () => {
           <div className={styles["booking-code"]}>
             <div>
               <h3>Booking</h3>
-              <span>123456789</span>
+              <span>{currentBooking.id}</span>
             </div>
             <span>Check your booking data:</span>
           </div>
@@ -42,34 +45,70 @@ const MyBooking = () => {
             <div className={styles["container--content__input"]}>
               <div className="container-input">
                 <label htmlFor="name">Name</label>
-                <input type="text" disabled />
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  disabled
+                  value={currentBooking.room.name}
+                />
               </div>
               <div className="container-input">
                 <label htmlFor="price-per-night">Price per night</label>
-                <input type="text" disabled />
+                <input
+                  type="text"
+                  id="price"
+                  name="price"
+                  disabled
+                  value={formattedPriceUSD(currentBooking.room.price)}
+                />
               </div>
             </div>
 
             <div className={styles["container--content__extras"]}>
               <strong>Extras</strong>
               <div className="container-checkbox">
-                <input type="checkbox" id="wifi" name="wifi" />
-                <label htmlFor="wifi">Wi-Fi</label>
+                <input
+                  type="checkbox"
+                  id="wifi"
+                  name="wifi"
+                  value="wifi"
+                  checked={currentBooking.room.hasWifi}
+                  disabled
+                />
+                <label htmlFor="wifi">WiFi</label>
               </div>
               <div className="container-checkbox">
                 <input
                   type="checkbox"
                   id="air-conditioning"
                   name="air-conditioning"
+                  value="air-conditioning"
+                  checked={currentBooking.room.hasAir}
+                  disabled
                 />
                 <label htmlFor="air-conditioning">Air conditioning</label>
               </div>
               <div className="container-checkbox">
-                <input type="checkbox" id="kitchen" name="kitchen" />
+                <input
+                  type="checkbox"
+                  id="kitchen"
+                  name="kitchen"
+                  value="kitchen"
+                  checked={currentBooking.room.hasKitchen}
+                  disabled
+                />
                 <label htmlFor="kitchen">Kitchen</label>
               </div>
               <div className="container-checkbox">
-                <input type="checkbox" id="pet-friendly" name="pet-friendly" />
+                <input
+                  type="checkbox"
+                  id="pet-friendly"
+                  name="pet-friendly"
+                  value="pet-friendly"
+                  checked={currentBooking.room.isPetFriendly}
+                  disabled
+                />
                 <label htmlFor="pet-friendly">Pet friendly</label>
               </div>
             </div>
